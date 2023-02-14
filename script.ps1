0b01573dac60645ac031f85a15f6fd394904b16e
Alexey Ivashkin <55810251+RomDmitriy@users.noreply.github.com>
2023-02-14 23:00:20 +0300

    v1.1.2
    Исправлен баг, из-за которого после выполнения программы запрашивались некие параметры

--- script.ps1
+++ script.ps1
@@ -8,41 +8,37 @@ param (
     $endIndex
 )
 
-function Merge-Video-Audio-Subtitle ([int]$start, [int]$end) {
-    Write-Output "Created by Alexey Ivashkin. v1.1.1"
+"Created by Alexey Ivashkin. v1.1.2"
 
-    $originalAudioParam = "-A"
+$originalAudioParam = "-A"
 
-    for ($i = $start; $i -le $end; $i++) {
-        # обработчик если номер серии всегда состоит из двух цифр
-        # закомментировать или изменить количествно нулей по необходимости
-        if ($i -lt 10) {
-            $number = "0$i"
-        } else {
-            $number = $i
-        }
-
-        # путь до файла вывода
-        $outPath = "D:\Titles\Alchemist/Fullmetal.Alchemist.Brotherhood.2009.E$number.mkv"
-        # путь до видеоряда
-        $videoPath = "D:\Titles\Fullmetal.Alchemist.Brotherhood.2009.MVO.STEPonee\Fullmetal.Alchemist.Brotherhood.2009.E$number.MVO.STEPonee.mp4"
-        # путь до аудиофайла
-        $audioPath = "D:\Titles\[mottoj] Fullmetal Alchemist Brotherhood (BDRip 1280x720 HEVC AAC)\[Audio]\[mottoj] Fullmetal Alchemist Brotherhood - $number (BDRip 1280x720 HEVC AAC).СВ-Дубль.mka"
-        # путь до файла субтитров
-        $subtitlePath = "D:\Titles\Fullmetal.Alchemist.Brotherhood.2009.MVO.STEPonee\Fullmetal.Alchemist.Brotherhood.2009.E$number.MVO.STEPonee.ass"
-
-        $progress = [math]::Round((($number - $start)/($end - $start + 1)) * 100, 1)
-        Write-Progress -Activity "Working.." -Status "$progress%           Merging in $outPath..." -PercentComplete $progress
-
-        # сам merge
-        # из-за специфики запуска программ через *это*, настройки через параметры не удались. Поэтому всё делаем ручками.
-        # если нужна оригинальная аудиодорожка, убираем "$originalAudioParam"
-        # если не нужен видеоряд, то убираем "$originalAudioParam $videoPath" (да, оба)
-        # если не нужно внешнее аудио, то убираем "$audioPath"
-        # если не нужны внешние субтитры, то убираем "$subtitlePath"
-        .\mkvmerge.exe -q -o $outPath $originalAudioParam $videoPath $audioPath $subtitlePath
+for ($i = $startIndex; $i -le $endIndex; $i++) {
+    # обработчик если номер серии всегда состоит из двух цифр
+    # закомментировать или изменить количествно нулей по необходимости
+    if ($i -lt 10) {
+        $number = "0$i"
+    } else {
+        $number = $i
     }
-    Write-Progress -Completed
-}
 
-Merge-Video-Audio-Subtitle -start $startIndex -end $endIndex
+    # путь до файла вывода
+    $outPath = "D:\Titles\Alchemist/Fullmetal.Alchemist.Brotherhood.2009.E$number.mkv"
+    # путь до видеоряда
+    $videoPath = "D:\Titles\Fullmetal.Alchemist.Brotherhood.2009.MVO.STEPonee\Fullmetal.Alchemist.Brotherhood.2009.E$number.MVO.STEPonee.mp4"
+    # путь до аудиофайла
+    $audioPath = "D:\Titles\[mottoj] Fullmetal Alchemist Brotherhood (BDRip 1280x720 HEVC AAC)\[Audio]\[mottoj] Fullmetal Alchemist Brotherhood - $number (BDRip 1280x720 HEVC AAC).СВ-Дубль.mka"
+    # путь до файла субтитров
+    $subtitlePath = "D:\Titles\Fullmetal.Alchemist.Brotherhood.2009.MVO.STEPonee\Fullmetal.Alchemist.Brotherhood.2009.E$number.MVO.STEPonee.ass"
+
+    $progress = [math]::Round((($number - $startIndex) / ($endIndex - $startIndex + 1)) * 100, 1)
+    Write-Progress -Activity "Working.." -Status "$progress%           Merging in $outPath..." -PercentComplete $progress
+
+    # сам merge
+    # из-за специфики запуска программ через *это*, настройки через параметры не удались. Поэтому всё делаем ручками.
+    # если нужна оригинальная аудиодорожка, убираем "$originalAudioParam"
+    # если не нужен видеоряд, то убираем "$originalAudioParam $videoPath" (да, оба)
+    # если не нужно внешнее аудио, то убираем "$audioPath"
+    # если не нужны внешние субтитры, то убираем "$subtitlePath"
+    .\mkvmerge.exe -q -o $outPath $originalAudioParam $videoPath $audioPath $subtitlePath
+}
+Write-Progress -Activity "Ready!" -Completed
