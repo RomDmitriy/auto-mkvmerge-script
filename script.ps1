314ee90a60cf77c1a6b8b286ca10f467131d8e2c
RomDmitriy <aisek72@gmail.com>
2023-02-14 21:42:05 +0300

    v1.1 Исправлен баг, что последний индекс был не включительно Добавлено оформление через progress-bar Убраны нерабочие параметры Добавлена инструкция по входным параметрам

--- script.ps1
+++ script.ps1
@@ -1,39 +1,51 @@
-﻿param ($startIndex, $endIndex)
+﻿param (
+    [Parameter(Mandatory=$true)]
+    [int]
+    $startIndex,
 
-function Merge-Video-Audio-Subtitle ([int]$start, [int]$end, [boolean]$mergeVideo = $true, [string]$mergeAudio = $true, [string]$mergeSubtitle = $true, [boolean]$noOriginalAudio = $true) {
-    echo "Created by Alexey Ivashkin."
-    for ($i=$start; $i -lt $end; $i++) {
+    [Parameter(Mandatory=$true)]
+    [int]
+    $endIndex
+)
+
+function Merge-Video-Audio-Subtitle ([int]$start, [int]$end) {
+    Write-Output "Created by Alexey Ivashkin. v1.1"
+
+    $originalAudioParam = "-A"
+
+    if (-not $mergeAudio) {
+        Write-Output "Video or audio must be provided!"
+        exit
+    }
+
+    for ($i = $start; $i -le $end; $i++) {
+        # обработчик если номер серии всегда состоит из двух цифр
+        # закомментировать или изменить количествно нулей по необходимости
         if ($i -lt 10) {
             $number = "0$i"
         } else {
             $number = $i
         }
 
-        $out = "out/Fullmetal.Alchemist.Brotherhood.2009.E$number.mkv"
-        $video = "D:\Titles\Fullmetal.Alchemist.Brotherhood.2009.MVO.STEPonee\Fullmetal.Alchemist.Brotherhood.2009.E$number.MVO.STEPonee.mp4"
-        $audio = "D:\Titles\[mottoj] Fullmetal Alchemist Brotherhood (BDRip 1280x720 HEVC AAC)\[Audio]\[mottoj] Fullmetal Alchemist Brotherhood - $number (BDRip 1280x720 HEVC AAC).СВ-Дубль.mka"
-        $subtitle = "D:\Titles\Fullmetal.Alchemist.Brotherhood.2009.MVO.STEPonee\Fullmetal.Alchemist.Brotherhood.2009.E$number.MVO.STEPonee.ass"
+        # путь до файла вывода
+        $outPath = "D:\mkvtoolnix\out/Fullmetal.Alchemist.Brotherhood.2009.E$number.mkv"
+        # путь до видеоряда
+        $videoPath = "D:\Titles\Fullmetal.Alchemist.Brotherhood.2009.MVO.STEPonee\Fullmetal.Alchemist.Brotherhood.2009.E$number.MVO.STEPonee.mp4"
+        # путь до аудиофайла
+        $audioPath = "D:\Titles\[mottoj] Fullmetal Alchemist Brotherhood (BDRip 1280x720 HEVC AAC)\[Audio]\[mottoj] Fullmetal Alchemist Brotherhood - $number (BDRip 1280x720 HEVC AAC).СВ-Дубль.mka"
+        # путь до файла субтитров
+        $subtitlePath = "D:\Titles\Fullmetal.Alchemist.Brotherhood.2009.MVO.STEPonee\Fullmetal.Alchemist.Brotherhood.2009.E$number.MVO.STEPonee.ass"
 
-        $progress = [math]::Round((($number - $start)/($end - $start)) * 100, 1)
-        Write-Progress -Activity "Working.." -Status "$progress%                Merging in $out..." -PercentComplete $progress
+        $progress = [math]::Round((($number - $start)/($end - $start + 1)) * 100, 1)
+        Write-Progress -Activity "Working.." -Status "$progress%           Merging in $outPath..." -PercentComplete $progress
 
-        if (-not ($mergeVideo)) {
-            $video = ""
-        }
-
-        if (-not($mergeAudio)) {
-            $audio = ""
-        }
-
-        if (-not ($mergeSubtitle)) {
-            $subtitle = ""
-        }
-
-        if ($noOriginalAudio) {
-            $originalAudioParameter = "-A"
-        }
-
-        .\mkvmerge.exe -q -o $out $originalAudioParameter $video $audio $subtitle
+        # сам merge
+        # из-за специфики запуска программ через *это*, настройки через параметры не удались. Поэтому всё делаем ручками.
+        # если нужна оригинальная аудиодорожка, убираем "$originalAudioParam"
+        # если не нужен видеоряд, то убираем "$originalAudioParam $videoPath" (да, оба)
+        # если не нужно внешнее аудио, то убираем "$audioPath"
+        # если не нужны внешние субтитры, то убираем "$subtitlePath"
+        .\mkvmerge.exe -q -o $outPath $originalAudioParam $videoPath $audioPath $subtitlePath
     }
     Write-Progress -Completed
 }
