12212d8c7d36dd18292dca1cdce5578cd160dcc0
Alexey Ivashkin <55810251+RomDmitriy@users.noreply.github.com>
2023-11-09 20:13:10 +0300

    ver 1.2
    Добавлена поддержка нескольких аудио и субтитров.
    Добавлено указание языков аудио и субтитров.

--- script.ps1
+++ script.ps1
@@ -8,7 +8,7 @@ param (
     $endIndex
 )
 
-$originalAudioParam = "-A"
+$removeOriginalSound = "-A"
 
 for ($i = $startIndex; $i -le $endIndex; $i++) {
     # обработчик если номер серии всегда состоит из двух цифр
@@ -20,23 +20,27 @@ for ($i = $startIndex; $i -le $endIndex; $i++) {
     }
 
     # путь до файла вывода
-    $outPath = "D:\Titles\Alchemist/Fullmetal.Alchemist.Brotherhood.2009.E$number.mkv"
+    $outPath = "D:\Titles\Haiyore! Nyaruko-san W\[Yousei-raws] Haiyore! Nyaruko-san W $number [BDrip 1920x1080 x264 FLAC].mkv"
     # путь до видеоряда
-    $videoPath = "D:\Titles\Fullmetal.Alchemist.Brotherhood.2009.MVO.STEPonee\Fullmetal.Alchemist.Brotherhood.2009.E$number.MVO.STEPonee.mp4"
-    # путь до аудиофайла
-    $audioPath = "D:\Titles\[mottoj] Fullmetal Alchemist Brotherhood (BDRip 1280x720 HEVC AAC)\[Audio]\[mottoj] Fullmetal Alchemist Brotherhood - $number (BDRip 1280x720 HEVC AAC).СВ-Дубль.mka"
+    $videoPath = "D:\Titles\Haiyore! Nyaruko-san W [BDRip] [1080p]\[Yousei-raws] Haiyore! Nyaruko-san W $number [BDrip 1920x1080 x264 FLAC].mkv"
+    # путь до аудиофайлов
+    $audioPathPrimary = "D:\Titles\Haiyore! Nyaruko-san W [BDRip] [1080p]\RUS Sound\[ShadMG & Лизавета]\[Yousei-raws] Haiyore! Nyaruko-san W $number [BDrip 1920x1080 x264 FLAC].mka"
+    $audioPathSecondary = "D:\Titles\Haiyore! Nyaruko-san W [BDRip] [1080p]\RUS Sound\[ShadMG & Лизавета]\[Yousei-raws] Haiyore! Nyaruko-san W $number [BDrip 1920x1080 x264 FLAC].mka"
     # путь до файла субтитров
-    $subtitlePath = "D:\Titles\Fullmetal.Alchemist.Brotherhood.2009.MVO.STEPonee\Fullmetal.Alchemist.Brotherhood.2009.E$number.MVO.STEPonee.ass"
+    $subtitlePath = "D:\Titles\Haiyore! Nyaruko-san W [BDRip] [1080p]\RUS Subs\[Yousei-raws] Haiyore! Nyaruko-san W $number [BDrip 1920x1080 x264 FLAC].ass"
+    $subtitleInscriptionsPath = "D:\Titles\Haiyore! Nyaruko-san W [BDRip] [1080p]\RUS Subs\надписи\[Yousei-raws] Haiyore! Nyaruko-san W $number [BDrip 1920x1080 x264 FLAC].ass"
 
     $progress = [math]::Round((($number - $startIndex) / ($endIndex - $startIndex + 1)) * 100, 1)
     Write-Progress -Activity "Working.." -Status "$progress%           Merging in $outPath..." -PercentComplete $progress
 
     # сам merge
     # из-за специфики запуска программ через *это*, настройки через параметры не удались. Поэтому всё делаем ручками.
-    # если нужна оригинальная аудиодорожка, убираем "$originalAudioParam"
-    # если не нужен видеоряд, то убираем "$originalAudioParam $videoPath" (да, оба)
-    # если не нужно внешнее аудио, то убираем "$audioPath"
-    # если не нужны внешние субтитры, то убираем "$subtitlePath"
-    .\mkvmerge.exe -q -o $outPath $originalAudioParam $videoPath $audioPath $subtitlePath
+    # если нужна оригинальная аудиодорожка, убираем "$removeOriginalSound"
+    # если не нужно внешнее аудио, то убираем "--language 0:rus $audioPathPrimary"
+    # если нужна лишь одна аудиодорожка (только primary), то убираем "--language 0:eng $audioPathSecondary"
+    # если не нужны внешние субтитры, то убираем "--language 0:rus --track-name 0:"Субтитры" $subtitlePath"
+    # если не нужны надписи, то убираем "--language 0:rus --track-name 0:"Надписи" $subtitleInscriptionsPath"
+    # P.S. Изначально выбираемой дорожкой становится аудио, которое указано в параметрах раньше, а субтитры - последними
+    .\mkvmerge.exe -q -o $outPath $removeOriginalSound $videoPath --language 0:rus $audioPathPrimary --language 0:eng $audioPathSecondary --language 0:rus --track-name 0:"Субтитры" $subtitlePath --language 0:rus --track-name 0:"Надписи" $subtitleInscriptionsPath
 }
 Write-Progress -Activity "Ready!" -Completed
